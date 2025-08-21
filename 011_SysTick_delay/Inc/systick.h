5c92665a0fb5f702417000bbd22f461f891adf22
Jogindhiran K J <99534942+Jogindhiran@users.noreply.github.com>
2025-08-21 15:10:33 +0530

    Add files via upload

---
+++ 011_SysTick_delay/Inc/systick.h
@@ -0,0 +1,14 @@
+/*
+ * systick.h
+ *
+ *  Created on: May 6, 2025
+ *      Author: Jogindhiran
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMS(int delay);
+
+
+#endif /* SYSTICK_H_ */
